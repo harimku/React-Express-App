ebbb653519d5d0d3ef3c9efc60853d51cbeb7131
Ha Rim Ku <hrku19@gmail.com>
2020-08-20 14:02:51 -0700

    Initialize front-end with create-react-app/ initialize back-end with Express Generator / connect the two

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
-
+    //initialize default state
+    constructor(props) {
+        super(props);
+        this.state = { apiResponse: "" };
+    }
+    
+    //fetch data from the API and store the response to 'this.state.apiResponse'
+    callAPI() {
+        fetch("http://localhost:9000/testAPI")
+            .then(res => res.text())
+            .then(res => this.setState({ apiResponse: res }));
+    }
+    
+    //React lifecycle method; executes callAPI method after the component mounts
+    componentWillMount() {
+        this.callAPI();
+    }
 
     render() {
         return (
@@ -12,6 +28,7 @@ class App extends Component {
                     <img src={logo} className="App-logo" alt="logo" />
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
+                <p className="App-intro">;{this.state.apiResponse}</p>
             </div>
         );
     }
