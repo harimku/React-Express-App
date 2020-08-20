ca0592716a480c597330d958040083d3a65c03a7
Ha Rim Ku <hrku19@gmail.com>
2020-08-20 13:47:59 -0700

    test edit

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { apiResponse: "" };
-    }
 
-    callAPI() {
-        fetch("http://localhost:9000/testAPI")
-            .then(res => res.text())
-            .then(res => this.setState({ apiResponse: res }))
-            .catch(err => err);
-    }
-
-    componentDidMount() {
-        this.callAPI();
-    }
 
     render() {
         return (
@@ -26,7 +12,6 @@ class App extends Component {
                     <img src={logo} className="App-logo" alt="logo" />
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
-                <p className="App-intro">{this.state.apiResponse}</p>
             </div>
         );
     }
